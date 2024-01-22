8fd1058f06e17d2dd1b57a53a13497a3380210bf
iaio998 <depalatisiacopo@gmail.com>
2024-01-22 15:38:18 +0100

    Added footer style

--- src/components/partials/AppFooter.vue
+++ src/components/partials/AppFooter.vue
@@ -1,9 +1,29 @@
 <template>
-  <div>Footer</div>
+  <div class="bg-dark">
+    <p class="m-0 p-0">Copyright © 2024 by Iacopo De Palatis</p>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "AppFooter",
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {},
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+div {
+  height: 5vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  p {
+    color: white;
+  }
+}
+</style>
